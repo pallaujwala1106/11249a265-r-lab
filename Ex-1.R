cf1da227ce50671df026e505d01cc95a6ee3b019
pallaujwala1106 <11249A265@kanchiuniv.ac.in>
2026-04-23 14:52:06 +0530

    Create Ex-1.R

---
+++ Ex-1.R
@@ -0,0 +1,91 @@
+1.1
+Student Information System
+R Program:
+roll_no <- 21 # Integer
+name <- "ujwala" # Character
+cgpa <- 8.45 # Numeric
+pass <- TRUE # Logical
+
+1.2
+Employee Salary Record
+R program:
+emp_id <- 5001
+salary <- 45000.75
+department <- "IT"
+is_permanent <- TRUE
+emp_id
+salary
+department
+is_permanent
+
+1.3
+Online Shopping Cart
+R program:
+product_name <- "Laptop"
+price <- 55000
+stock_available <- 21
+discount_eligible <- FALSE
+product_name
+price
+stock_available
+discount_eligible
+
+1.4
+Weather Monitoring System
+R program:
+city <- "banglore"
+temperature <- 36.5
+rain <- FALSE
+wind_speed <- 12.8
+city
+temperature
+rain
+wind_speed
+
+1.5
+Examination Result Processing
+R program:
+subject <- "Physics"
+total_marks <- 100
+marks_obtained <- 78
+result <- marks_obtained >= 40
+subject
+total_marks
+marks_obtained
+result
+
+1.6
+Banking Application
+R program:
+account_no <- 123456789
+holder_name <- "Neelima"
+balance <- 21000.50
+account_active <- TRUE
+account_no
+holder_name
+balance
+account_active
+
+1.7
+Movie Rating System
+R program:
+movie_title <- "Avengers age of ultron"
+rating <- 9.5
+reviews <- 1200
+available <- TRUE
+movie_title
+rating
+reviews
+available
+
+1.8
+Traﬃc Monitoring System
+R program:
+vehicle_count <- 210
+location <- "Highway Road"
+avg_speed <- 21.6
+congested <- TRUE
+vehicle_count
+location
+avg_speed
+congested
